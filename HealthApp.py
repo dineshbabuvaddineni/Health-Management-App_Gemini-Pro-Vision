5ebef9b8f0642ee88a2fa38814f1488da9e5ab87
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2024-01-19 10:00:02 +0530

    first commit

--- HealthApp.py
+++ HealthApp.py
@@ -1,20 +1,25 @@
+#Health Management Application -->
+#It calculates the amount of calories we are consuming in a day from the food we will take as breakfast ,lunch and dinner
+
+#load all the environment variables
 from dotenv import load_dotenv
-load_dotenv() ## load all the environment variables from .env
+load_dotenv() 
 
 import streamlit as st
 import os
-from PIL import Image
 import google.generativeai as genai
+from PIL import Image
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Load Gemini pro vision model
-model=genai.GenerativeModel('gemini-pro-vision')
+##Function to load Google Gemini Pro Vision and get response
 
-def get_gemini_response(input,image,user_prompt):
-    response=model.generate_content([input,image[0],user_prompt])
+def get_gemini_response(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
     return response.text
 
+#Input Image setup
 def input_image_details(uploaded_file):
     if uploaded_file is not None:
         # Read the file into bytes
@@ -29,13 +34,11 @@ def input_image_details(uploaded_file):
         return image_parts
     else:
         raise FileNotFoundError("No file uploaded")
+    
+#Intialize our streamlit app
 
-
-##initialize our streamlit app
-
-st.set_page_config(page_title="MultiLanguage Invoice Extractor")
-
-st.header("MultiLanguage Invoice Extractor")
+st.set_page_config(page_title="Gemini Health App")
+st.header("Gemini Health App")
 input=st.text_input("Input Prompt: ",key="input")
 uploaded_file = st.file_uploader("Choose an image of the invoice...", type=["jpg", "jpeg", "png"])
 
@@ -43,17 +46,28 @@ if uploaded_file is not None:
     image=Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
-submit=st.button("Tell me about the invoice")
+submit=st.button("Tell me the total calories")
 
 input_prompt="""
-You are an expert in understanding invoices. We will upload a a image as invoice
-and you will have to answer any questions based on the uploaded invoice image
-"""
+You are an expert in nutrionist where you need to see the food items from the image and 
+        calculate the total calories, also provide the details of every food items with 
+        calories intake format is below  format.
 
-## if submit button is clicked
+        1.Item 1 - no of calories
+        2.Item 2 - no of calories
+
+------
+------
+
+"""
+#If submit button is clicked
 
 if submit:
     image_data=input_image_details(uploaded_file)
     response=get_gemini_response(input_prompt,image_data,input)
-    st.subheader("The Rresponse is")
+    st.subheader("The response is :")
     st.write(response)
+
+
+
+
